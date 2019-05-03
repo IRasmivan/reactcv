b57a490391a81ff4deba2fadac1277da9c4e3551
CEX <cex@cexs-air-2.mynet>
2019-05-03 01:37:52 +0100

    not jenkin

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,5 +1,5 @@
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+docker stop $(docker ps --filter ancestor=reactcv -a -q)
+docker rm $(docker ps --filter ancestor=reactcv -a -q)
 docker build . -t reactcv
 docker images
 docker run -d -p 80:80 reactcv

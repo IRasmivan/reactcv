d18ef88120a0c2946684952f461af311b189efa4
CEX <cex@cexs-air-2.mynet>
2019-05-04 18:25:35 +0100

    Added commit

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,7 +1,9 @@
-docker stop $(docker ps --filter ancestor=reactcv -a -q)
-docker rm $(docker ps --filter ancestor=reactcv -a -q)
-docker rmi $(docker images -f "dangling=true" -q)
-docker rmi $(docker images |grep 'reactcv')
+docker stop $(docker ps --filter ancestor=reactcv -a -q) #stop reactcv container
+docker rm $(docker ps --filter ancestor=reactcv -a -q) #remove reactcv container
+docker rmi $(docker images |grep 'reactcv') #remove reactcv image
+docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs sudo docker rm #remove all the exit container
+docker rmi $(docker images -f "dangling=true" -q) #remove all dangling image
+
 docker build . -t reactcv
 docker images
 docker run -d -p 80:80 reactcv

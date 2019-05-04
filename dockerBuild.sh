693664f74a37a8b0bc0e4dfffe9337223d0f1fac
CEX <cex@cexs-air-2.mynet>
2019-05-04 18:21:56 +0100

    Docker image removel fix

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,5 +1,7 @@
 docker stop $(docker ps --filter ancestor=reactcv -a -q)
 docker rm $(docker ps --filter ancestor=reactcv -a -q)
+docker rmi $(docker images -f "dangling=true" -q)
+docker rmi $(docker images |grep 'reactcv')
 docker build . -t reactcv
 docker images
 docker run -d -p 80:80 reactcv

0a1a41c8c2506762e87b7ff3d3e6d7ad7e1756d3
CEX <cex@cexs-air-2.mynet>
2019-05-05 10:09:10 +0100

    Updates

---
+++ precache-manifest.1f9c17fc274a1fbe90c9a001408b8c47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56f8b14731d0ba923fb4",
+    "url": "/reactcv/static/js/runtime~main.56f8b147.js"
+  },
+  {
+    "revision": "06c9a4f9bec62aa6d7fc",
+    "url": "/reactcv/static/js/main.06c9a4f9.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/reactcv/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "06c9a4f9bec62aa6d7fc",
+    "url": "/reactcv/static/css/main.2069a932.chunk.css"
+  },
+  {
+    "revision": "b4b8c006d7cf62b7a50cb95d57d9e7be",
+    "url": "/reactcv/index.html"
+  }
+];

d8796319c0d37fcc9f86ec45e11364ea4b59c534
CEX <cex@cexs-air-2.mynet>
2019-05-05 02:05:31 +0100

    docker updated

--- Dockerfile
+++ Dockerfile
@@ -12,10 +12,9 @@ COPY --from=react-build /app/build /usr/share/nginx/html
 
 
 ## Install Certificate -- Start
-RUN apt-get update && \
-    apt-get -y install sudo
 RUN apk add netcat-openbsd bc curl wget git bash
 RUN apk add libressl
+RUN apk sudo
 RUN git clone https://github.com/Neilpang/acme.sh.git
 RUN acme.sh/acme.sh --install
 RUN ./acme.sh/acme.sh --issue -d rasmivan.com -w /var/www/rasmivan.com
--- dockerBuild.sh
+++ dockerBuild.sh
@@ -2,7 +2,7 @@ docker stop $(docker ps --filter ancestor=reactcv -a -q) #stop reactcv container
 docker rm $(docker ps --filter ancestor=reactcv -a -q) #remove reactcv container
 docker rmi $(docker images |grep 'reactcv') #remove reactcv image
 docker ps -a | grep Exit | cut -d ' ' -f 1 | xargs sudo docker rm #remove all the exit container
-docker rmi $(docker images -f "dangling=true" -q) #remove all dangling image
+docker rmi -f $(docker images -f "dangling=true" -q) #remove all dangling image
 
 docker build . -t reactcv
 docker images

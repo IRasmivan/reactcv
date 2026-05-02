4562203ea920f5f336eb0dc9f66761f795ed8425
Rasmivan Ilangovan <rasmivan.ilangovan@itv.com>
2026-05-03 00:41:12 +0100

    Updated the color theam

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -2,6 +2,25 @@ import React, { Component } from 'react'
 
 export default class Blog extends Component {
   render() {
+    const posts = [
+      {
+        date: "May 11, 2019",
+        category: "CV in React",
+        title: "Build your CV using React",
+        description: "This blog is to walk you through to build your CV using react.",
+        image: "images/blog-1.png",
+        url: "https://blog.rasmivan.com/"
+      },
+      {
+        date: "May 12, 2019",
+        category: "Blog Design",
+        title: "Building a Blog",
+        description: "This blog is to walk you through to build your own Blog site using style with bulma and blog content from contentful.",
+        image: "images/blog-2.png",
+        url: "https://blog.rasmivan.com/"
+      }
+    ];
+
     return (
       <div>
 	        <section id="blog" className="colorlib-blog" data-section="blog">
@@ -12,28 +31,35 @@ export default class Blog extends Component {
 					<h2 className="colorlib-heading">Recent Blog</h2>
 					</div>
 				</div>
-				<div className="row">
-					<div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
-					<div className="blog-entry">
-						<a href="https://blog.rasmivan.com/" className="blog-img"><img src="images/blog-1.png" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
-						<div className="desc">
-							<span><small>May 11, 2019 </small> | <small> CV in React </small> </span>
-							<h3><a href="https://blog.rasmivan.com/">Build your CV using React</a></h3>
-							<p>This blog is to walk you through to build your CV using react.</p>
+				<div className="row blog-grid-row">
+					{posts.map((post, index) => (
+						<div
+							className="col-md-6 col-sm-6 animate-box"
+							data-animate-effect={index === 0 ? "fadeInLeft" : "fadeInRight"}
+							key={post.title}
+						>
+							<article className="blog-entry">
+								<a href={post.url} className="blog-img" target="_blank" rel="noopener noreferrer">
+									<img src={post.image} className="img-responsive" alt={post.title} />
+								</a>
+								<div className="desc">
+									<div className="blog-meta">
+										<span className="blog-date">{post.date}</span>
+										<span className="blog-tag">{post.category}</span>
+									</div>
+									<h3>
+										<a href={post.url} target="_blank" rel="noopener noreferrer">
+											{post.title}
+										</a>
+									</h3>
+									<p>{post.description}</p>
+									<a className="blog-link" href={post.url} target="_blank" rel="noopener noreferrer">
+										Read article <i className="icon-arrow-right3" aria-hidden="true" />
+									</a>
+								</div>
+							</article>
 						</div>
-					</div>
-					</div>
-					<div className="col-md-4 col-sm-6 animate-box" data-animate-effect="fadeInRight">
-					<div className="blog-entry">
-						<a href="https://blog.rasmivan.com/" className="blog-img"><img src="images/blog-2.png" className="img-responsive" alt="HTML5 Bootstrap Template by colorlib.com" /></a>
-						<div className="desc">
-							<span><small>May 12, 2019 </small> | <small> Blog Design </small> </span>
-							<h3><a href="https://blog.rasmivan.com/">Building a Blog </a></h3>
-							<p>This blog is to walk you through to build your own Blog site using style with bulma and blog content from contentful.</p>
-						</div>
-					</div>
-					</div>
-					
+					))}
 				</div>
 				{/* <div className="row">
 					<div className="col-md-12 animate-box">
